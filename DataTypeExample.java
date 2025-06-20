206f13a1ca0c46c21f09bb96f5401d972991e9c6
Gayatri Barhate <barhategayatri40@gmail.com>
2025-06-20 08:15:55 +0530

    Create DataTypeExample.java

---
+++ DataTypeExample.java
@@ -0,0 +1,33 @@
+public class DataTypeExample {
+    public static void main(String[] args) {
+        // Integer types
+        byte a = 25;
+        short b = 32000;
+        int c = 250000;
+        long d = 9876543210L;
+
+        // Floating-point types
+        float e = 3.14f;
+        double f = 123.456;
+
+        // Character type
+        char g = 'Z';
+
+        // Boolean type
+        boolean h = false;
+
+        // String type
+        String name = "Hello, Java!";
+
+        // Output the new values
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
+        System.out.println("String: " + name);
+    }
+}
