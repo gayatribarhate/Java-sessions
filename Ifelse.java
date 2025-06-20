b1e4452455973073ae03bb033559be56477194ca
Gayatri Barhate <barhategayatri40@gmail.com>
2025-06-20 08:08:31 +0530

    Create Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int a = 75; // new input number
+
+        // Check whether 'a' is even or odd
+        if (a % 2 == 0) {
+            System.out.println("a is even");
+        } else {
+            System.out.println("a is odd");
+        }
+    }
+}
