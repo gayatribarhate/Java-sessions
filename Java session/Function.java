f72926a0bc91e96373a8381fe9505c7341eab39b
Gayatri Barhate <barhategayatri40@gmail.com>
2025-06-20 08:10:25 +0530

    Create Function.java

---
+++ Java session/Function.java
@@ -0,0 +1,11 @@
+public class Function {
+    // Function to multiply two numbers
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int pro = multiply(12, 8); // new values for multiplication
+        System.out.println("Product is: " + pro); // output the result
+    }
+}
