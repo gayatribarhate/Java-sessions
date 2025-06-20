59c04f92bd37a9bf9973a05eae9d53db8e73e13d
Gayatri Barhate <barhategayatri40@gmail.com>
2025-06-20 07:57:21 +0530

    Create relational.java

---
+++ relational.java
@@ -0,0 +1,12 @@
+public class Relational{Add commentMore actions
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 100;
+
+        if(a <= b) {
+            System.out.println("a is less than or equal to b");
+        } else {
+            System.out.println("a is greater than b");
+        }
+    }
+}
